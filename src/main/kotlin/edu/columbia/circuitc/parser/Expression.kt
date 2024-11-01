6b88d9534f7ab41c35c6d8040b859b7b0bbd6022
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2024-11-01 14:21:07 -0400

    Implement statement expression parsing

--- src/main/kotlin/edu/columbia/circuitc/parser/Expression.kt
+++ src/main/kotlin/edu/columbia/circuitc/parser/Expression.kt
@@ -5,7 +5,8 @@ interface Expression
 /**
  * Circuit expression, which is the top-level construct of the language.
  */
-data class CircuitExpression(private val name: String, private val args: ArgListExpression) : Expression
+data class CircuitExpression(private val name: String, private val args: ArgListExpression,
+                             private val statements: StatementListExpression) : Expression
 
 /**
  * Argument list expression.
@@ -16,3 +17,87 @@ data class ArgListExpression(val args: List<ArgumentExpression>) : Expression
  * Argument expression.
  */
 data class ArgumentExpression(private val bitWidth: Int, private val name: String, private val isInput: Boolean) : Expression
+
+/**
+ * Statement list expression.
+ */
+data class StatementListExpression(val statements: List<StatementExpression>) : Expression
+
+/**
+ * Statement expression.
+ */
+interface StatementExpression : Expression
+
+/**
+ * L-val expression, an expression that appears on the left-hand side.
+ */
+interface LValExpression: Expression
+
+/**
+ * R-val expression, an expression that appears on the right-hand side.
+ */
+interface RValExpression: Expression
+
+/**
+ * Operand of an expression.
+ */
+interface Operand: RValExpression
+
+/**
+ * Operand list expression.
+ */
+data class OperandListExpression(val operands: List<Operand>) : Expression
+
+data class NumericalOperand(private val value: Int): Operand
+data class IdentifierOperand(private val name: String): Operand, LValExpression
+
+enum class UnaryOp {
+    NOT
+}
+
+enum class BinOp {
+    AND, OR, XOR
+}
+
+/**
+ * Unary operand expression. OP RHS
+ */
+data class UnaryOpExpression(private val rhs: Operand, private val type: UnaryOp): RValExpression
+
+/**
+ * Binary operand expression. LHS OP RHS
+ */
+data class BinOpExpression(private val lhs: Operand, private val rhs: Operand, private val type: BinOp): RValExpression
+
+/**
+ * Ternary operand expression. COND ? TRUE-OP : FALSE-OP
+ */
+data class TernaryOpExpression(private val condition: Operand, private val trueOp: Operand,
+                               private val falseOp: Operand) : RValExpression
+
+/**
+ * Declaration expression -- declares a variable. It can be a lval of an assignment.
+ */
+interface DeclExpression: LValExpression
+
+/**
+ * Wire declaration expression.
+ */
+data class WireDeclExpression(private val bitWidth: Int, private val name: String) : DeclExpression
+
+/**
+ * Clock declaration expression.
+ */
+data class ClockDeclExpression(private val name: String) : DeclExpression, StatementExpression
+
+/**
+ * Register declaration expression.
+ */
+data class RegisterDeclExpression(
+    private val bitWidth: Int,
+    private val name: String, private val params: OperandListExpression) : DeclExpression, StatementExpression
+
+/**
+ * Assignment expression. LVAL = RVAL
+ */
+data class AssignmentExpression(private val lVal: LValExpression, private val rVal: RValExpression): StatementExpression
--- src/main/kotlin/edu/columbia/circuitc/parser/Parser.kt
+++ src/main/kotlin/edu/columbia/circuitc/parser/Parser.kt
@@ -11,7 +11,7 @@ class Parser {
     private fun parseProgram(tokens: List<Token>): CircuitExpression {
         var currTokens = tokens
 
-        // PROG -> CIRCUIT ID '(' ARG-LIST ')' '{' '}'
+        // PROG -> CIRCUIT ID '(' ARG-LIST ')' '{' STMT-LIST '}'
         val expectedLeftStructure = listOf(
             TokenType.CIRCUIT,
             TokenType.IDENTIFIER,
@@ -21,7 +21,6 @@ class Parser {
         val expectedRightStructure = listOf(
             TokenType.RIGHT_PAREN,
             TokenType.LEFT_BRACE,
-            TokenType.RIGHT_BRACE
         )
 
         validateStructure(currTokens, expectedLeftStructure)
@@ -32,7 +31,14 @@ class Parser {
         currTokens = nTokens
 
         validateStructure(currTokens, expectedRightStructure)
-        return CircuitExpression(circuitName, argList)
+
+        currTokens = currTokens.subList(expectedRightStructure.size, currTokens.size)
+        val (stmtList, remTokens) = parseStatementList(currTokens)
+
+        currTokens = remTokens
+        validateStructure(currTokens, listOf(TokenType.RIGHT_BRACE))
+
+        return CircuitExpression(circuitName, argList, stmtList)
     }
 
     private fun parseArgList(tokens: List<Token>): Pair<ArgListExpression, List<Token>> {
@@ -54,6 +60,8 @@ class Parser {
     }
 
     private fun parseArg(tokens: List<Token>): Pair<ArgumentExpression, List<Token>> {
+        // ARG -> IN  BITS '<' NUM '>' ID |
+        //        OUT BITS '<' NUM '>' ID
         val expectedStructure = listOf(
             TokenType.BITS,
             TokenType.LEFT_ANGLE,
@@ -95,14 +103,244 @@ class Parser {
             return emptyList<ArgumentExpression>() to tokens
         }
 
-        // COMMA-ARG-LIST -> ',' ARG ARG-LIST
+        // COMMA-ARG-LIST -> ',' ARG COMMA-ARG-LIST
         if (tokens[0].type != TokenType.COMMA) {
             unexpectedToken(tokens[0], TokenType.COMMA.text)
         }
 
         val (arg, nTokens) = parseArg(tokens.subList(1, tokens.size))
-        val (argList, remTokens) = parseArgList(nTokens)
-        return listOf(arg) + argList.args to remTokens
+        val (argList, remTokens) = parseCommaArgList(nTokens)
+        return listOf(arg) + argList to remTokens
+    }
+
+    private fun parseStatementList(tokens: List<Token>): Pair<StatementListExpression, List<Token>> {
+        if (tokens.isEmpty()) {
+            unexpectedEOF(TokenType.RIGHT_BRACE)
+        }
+
+        // STMT-LIST -> epsilon
+        if (tokens[0].type == TokenType.RIGHT_BRACE) {
+            return StatementListExpression(emptyList()) to tokens
+        }
+
+        // STMT-LIST -> STMT SEMI-STMT-LIST
+        val (arg, nTokens) = parseStatement(tokens)
+        val (argList, remTokens) = parseSemiStatementList(nTokens)
+
+        val allArgs = mutableListOf(arg) + argList
+        return StatementListExpression(allArgs) to remTokens
+    }
+
+    private fun parseStatement(tokens: List<Token>): Pair<StatementExpression, List<Token>> {
+        if (tokens.isEmpty()) {
+            unexpectedEOF(TokenType.IDENTIFIER)
+        }
+
+        // STMT -> LVAL | LVAL '=' RVAL
+        val (lVal: LValExpression, currTokens) = when(tokens[0].type) {
+            // LVAL -> BITS '<' NUM '>' ID
+            TokenType.BITS -> {
+                val expectedStructure = listOf(
+                    TokenType.BITS,
+                    TokenType.LEFT_ANGLE,
+                    TokenType.NUM,
+                    TokenType.RIGHT_ANGLE,
+                    TokenType.IDENTIFIER
+                )
+
+                validateStructure(tokens, expectedStructure)
+                WireDeclExpression(tokens[2].text.toInt(), tokens[4].text) to tokens.subList(expectedStructure.size, tokens.size)
+            }
+
+            // LVAL -> CLOCK ID
+            TokenType.CLOCK -> {
+                val expectedStructure = listOf(TokenType.CLOCK, TokenType.IDENTIFIER)
+                validateStructure(tokens, expectedStructure)
+
+                ClockDeclExpression(tokens[1].text) to tokens.subList(expectedStructure.size, tokens.size)
+            }
+
+            // LVAL -> REGISTER '<' NUM '>' ID '(' OP-LIST ')'
+            TokenType.REGISTER -> {
+                val expectedStructure = listOf(
+                    TokenType.REGISTER, TokenType.LEFT_ANGLE, TokenType.NUM, TokenType.RIGHT_ANGLE,
+                    TokenType.IDENTIFIER, TokenType.LEFT_PAREN
+                )
+                validateStructure(tokens, expectedStructure)
+
+                val (opList, nTokens) = parseOperandList(tokens.subList(expectedStructure.size, tokens.size))
+
+                validateStructure(nTokens, listOf(TokenType.RIGHT_PAREN))
+                RegisterDeclExpression(tokens[2].text.toInt(), tokens[4].text, opList) to nTokens.subList(1, nTokens.size)
+            }
+
+            // LVAL -> ID
+            TokenType.IDENTIFIER -> {
+                val (op, nTokens) = parseOperand(tokens)
+
+                if (op !is LValExpression) {
+                    unexpectedToken(tokens[0], TokenType.IDENTIFIER.text)
+                    throw Exception()
+                }
+
+                op to nTokens
+            }
+
+            else -> {
+                unexpectedToken(tokens[0], TokenType.IDENTIFIER.text)
+                throw Exception() // placate compiler
+            }
+        }
+
+        // These decl expressions don't allow assignment, so just return them
+        if (lVal is StatementExpression) {
+            return lVal as StatementExpression to currTokens
+        }
+
+        // '=' RVAL
+        validateStructure(currTokens, listOf(TokenType.EQUALS))
+        val (rVal, remTokens) = parseRValExpression(currTokens.subList(1, currTokens.size))
+
+        return AssignmentExpression(lVal, rVal) to remTokens
+    }
+
+    private fun parseSemiStatementList(tokens: List<Token>): Pair<List<StatementExpression>, List<Token>> {
+        if (tokens.isEmpty()) {
+            unexpectedEOF(TokenType.RIGHT_BRACE)
+        }
+
+        // SEMI-STMT-LIST -> epsilon
+        if (tokens[0].type == TokenType.RIGHT_BRACE) {
+            return emptyList<StatementExpression>() to tokens
+        }
+
+        if (tokens[0].type != TokenType.SEMICOLON) {
+            unexpectedToken(tokens[0], TokenType.SEMICOLON.text)
+        }
+
+        // SEMI-STMT-LIST -> ';' [FOLLOW(SEMI-STMT-LIST) = {'}'} => no more statements in the list]
+        if (tokens[1].type == TokenType.RIGHT_BRACE) {
+            return emptyList<StatementExpression>() to tokens.subList(1, tokens.size)
+        }
+
+        // SEMI-STMT-LIST -> ';' STMT SEMI-STMT-LIST
+        val (arg, nTokens) = parseStatement(tokens.subList(1, tokens.size))
+        val (argList, remTokens) = parseSemiStatementList(nTokens)
+        return listOf(arg) + argList to remTokens
+    }
+
+    private fun parseOperandList(tokens: List<Token>): Pair<OperandListExpression, List<Token>> {
+        if (tokens.isEmpty()) {
+            unexpectedEOF(TokenType.RIGHT_PAREN)
+        }
+
+        // OP-LIST -> epsilon
+        if (tokens[0].type == TokenType.RIGHT_PAREN) {
+            return OperandListExpression(emptyList()) to tokens
+        }
+
+        // OP-LIST -> OP COMMA-OP-LIST
+        val (arg, nTokens) = parseOperand(tokens)
+        val (argList, remTokens) = parseCommaOpList(nTokens)
+
+        return OperandListExpression(listOf(arg) + argList) to remTokens
+    }
+
+    private fun parseOperand(tokens: List<Token>): Pair<Operand, List<Token>> {
+        if (tokens.isEmpty()) {
+            unexpectedEOF(TokenType.IDENTIFIER);
+        }
+
+        // OP -> ID | NUM
+        return when (tokens[0].type) {
+            TokenType.IDENTIFIER -> IdentifierOperand(tokens[0].text) to tokens.subList(1, tokens.size)
+            TokenType.NUM -> NumericalOperand(tokens[0].text.toInt()) to tokens.subList(1, tokens.size)
+            else -> {
+                unexpectedToken(tokens[0], TokenType.IDENTIFIER.text)
+                throw Exception() // placate compiler
+            }
+        }
+    }
+
+    private fun parseCommaOpList(tokens: List<Token>): Pair<List<Operand>, List<Token>> {
+        if (tokens.isEmpty()) {
+            unexpectedEOF(TokenType.RIGHT_PAREN)
+        }
+
+        // COMMA-OP-LIST -> epsilon
+        if (tokens[0].type == TokenType.RIGHT_PAREN) {
+            return emptyList<Operand>() to tokens
+        }
+
+        // COMMA-OP-LIST -> ',' OP COMMA-OP-LIST
+        if (tokens[0].type != TokenType.COMMA) {
+            unexpectedToken(tokens[0], TokenType.COMMA.text)
+        }
+
+        val (arg, nTokens) = parseOperand(tokens.subList(1, tokens.size))
+        val (argList, remTokens) = parseCommaOpList(nTokens)
+        return listOf(arg) + argList to remTokens
+    }
+
+    private fun parseRValExpression(tokens: List<Token>): Pair<RValExpression, List<Token>> {
+        if (tokens.isEmpty()) {
+            unexpectedEOF(TokenType.IDENTIFIER)
+        }
+
+        if (tokens[0].type == TokenType.NOT) {
+            // RVAL -> NOT OP
+            val (op, nTokens) = parseOperand(tokens.subList(1, tokens.size))
+            return UnaryOpExpression(op, UnaryOp.NOT) to nTokens
+        } else {
+            val (op, nTokens) = parseOperand(tokens)
+
+            if (nTokens.isEmpty()) {
+                unexpectedEOF(TokenType.SEMICOLON)
+            }
+
+            // RVAL -> OP
+            if (nTokens[0].type == TokenType.SEMICOLON) {
+                return op to nTokens
+            }
+
+            return when(nTokens[0].type) {
+                // RVAL -> OP BIN-OP OP; BIN-OP -> AND | OR | XOR
+                TokenType.AND, TokenType.OR, TokenType.XOR -> {
+                    val (rhs, remTokens) = parseOperand(nTokens.subList(1, nTokens.size))
+                    BinOpExpression(op, rhs, toBinOpType(nTokens[0].type)) to remTokens
+                }
+
+                // RVAL -> OP '?' OP ':' OP
+                TokenType.QUESTION -> {
+                    val (trueOp, remTokens) = parseOperand(nTokens.subList(1, nTokens.size))
+
+                    if (remTokens.isEmpty()) {
+                        unexpectedEOF(TokenType.COLON)
+                    }
+
+                    if (remTokens[0].type != TokenType.COLON) {
+                        unexpectedToken(remTokens[0], TokenType.COLON.text)
+                    }
+
+                    val (falseOp, remTokensLeft) = parseOperand(remTokens.subList(1, remTokens.size))
+                    TernaryOpExpression(op, trueOp, falseOp) to remTokensLeft
+                }
+
+                else -> {
+                    unexpectedToken(nTokens[0], TokenType.AND.text)
+                    throw Exception() // placate compiler
+                }
+            }
+        }
+    }
+
+    private fun toBinOpType(tokenType: TokenType): BinOp {
+        return when (tokenType) {
+            TokenType.AND -> BinOp.AND
+            TokenType.OR -> BinOp.OR
+            TokenType.XOR -> BinOp.XOR
+            else -> throw IllegalStateException("converting non-operator token to binary operator")
+        }
     }
 
     private fun validateStructure(tokens: List<Token>, expectedStructure: List<TokenType>) {
