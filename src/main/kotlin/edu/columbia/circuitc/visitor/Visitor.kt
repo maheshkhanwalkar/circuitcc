87064ded5949eec41f5158d1e56f2233da6f51d7
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2024-11-23 10:06:40 -0500

    Add IRVisitor interface

--- src/main/kotlin/edu/columbia/circuitc/visitor/Visitor.kt
+++ src/main/kotlin/edu/columbia/circuitc/visitor/Visitor.kt
@@ -1,9 +1,10 @@
 package edu.columbia.circuitc.visitor
 
+import edu.columbia.circuitc.ir.*
 import edu.columbia.circuitc.parser.*
 
 /**
- * Visitor pattern.
+ * Visitor pattern for AST.
  */
 interface ASTVisitor<T> {
     fun visit(circuitExpression: CircuitExpression): T
@@ -20,4 +21,22 @@ interface ASTVisitor<T> {
     fun visit(numericalOperand: NumericalOperand): T
     fun visit(identifierOperand: IdentifierOperand): T
     fun visit(operandListExpression: OperandListExpression): T
+}
+
+/**
+ * Visitor pattern for IR.
+ */
+interface IRVisitor<T> {
+    fun visit(circuitValue: CircuitValue): T
+    fun visit(inputPinValue: InputPinValue): T
+    fun visit(outputPinValue: OutputPinValue): T
+    fun visit(registerValue: RegisterValue): T
+    fun visit(clkValue: ClockValue): T
+    fun visit(selectorValue: SelectorValue): T
+    fun visit(constantValue: ConstantValue): T
+    fun visit(tunnelValue: TunnelValue): T
+    fun visit(andGateValue: AndGateValue): T
+    fun visit(orGateValue: OrGateValue): T
+    fun visit(xorGateValue: XorGateValue): T
+    fun visit(notGateValue: NotGateValue): T
 }
